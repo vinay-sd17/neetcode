f578d55a1a05c6ea89c18191e7231bfbffb152ee
vinay-sd17 <vinay.desikavinayagam@gmail.com>
2022-10-28 23:52:47 +0530

    Added solutions

---
+++ com/neetcode/solutions/one/LongestCommonPrefix.java
@@ -0,0 +1,27 @@
+package com.neetcode.solutions.one;
+
+// https://leetcode.com/problems/longest-common-prefix/description/
+
+public class LongestCommonPrefix {
+
+  public static void main(String[] args) {
+    System.out.println(longestCommonPrefix(new String[] { "flower", "flow", "flight" }));
+    System.out.println(longestCommonPrefix(new String[] { "dog", "racecar", "car" }));
+    System.out.println(longestCommonPrefix(new String[] { "ab", "a" }));
+  }
+
+  public static String longestCommonPrefix(String[] strs) {
+    StringBuilder res = new StringBuilder();
+    for (int i = 0; i < strs[0].length(); i++) {
+      for (int j = 1; j < strs.length; j++) {
+        if (strs[j].length() == i || strs[0].charAt(i) != strs[j].charAt(i)) {
+          return res.toString();
+        }
+      }
+      res.append(strs[0].charAt(i));
+    }
+    return res.toString();
+
+  }
+
+}
--- com/neetcode/solutions/one/README.md
+++ com/neetcode/solutions/one/README.md
@@ -3,4 +3,7 @@
 1. https://leetcode.com/problems/contains-duplicate/
 2. https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
 3. https://leetcode.com/problems/valid-anagram/description/
-4. 
+4. https://leetcode.com/problems/is-subsequence/
+5. https://leetcode.com/problems/length-of-last-word/
+6. https://leetcode.com/problems/longest-common-prefix/description/
+7. https://leetcode.com/problems/two-sum/
